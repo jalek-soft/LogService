272d07987f9c6d821d1097d8a0cda0a68a5893e6
Yuliya Alekseeva <jalek@softserveinc.com>
2021-05-17 17:54:56 +0300

    Date

--- app/src/main/java/com/example/logservice/LogService.kt
+++ app/src/main/java/com/example/logservice/LogService.kt
@@ -6,30 +6,42 @@ import android.os.IBinder
 import android.util.Log
 
 import com.example.aidl.ILogAIDLInterface
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LogService : Service() {
 
     val TAG = LogService::class.java.simpleName
 
     override fun onBind(intent: Intent?): IBinder {
+
         return object : ILogAIDLInterface.Stub() {
+
             override fun log(loglevel: String, message: String) {
-                    when (loglevel) {
+                    val head  = Date(System.currentTimeMillis()).formatToServerTimeDefaults()+"====>"
+                when (loglevel) {
                         "v" -> {
-                            Log.v(TAG, message)
+                            Log.v(TAG, head+message)
                         }
                         "d" -> {
-                            Log.d(TAG, message)
+                            Log.d(TAG, head+message)
                         }
                         "i" -> {
-                            Log.i(TAG, message)
+                            Log.i(TAG, head+message)
                         }
                         "w" -> {
-                            Log.w(TAG, message)
+                            Log.w(TAG, head+message)
                         }
-                        else -> Log.e(TAG, message)
+                        else -> Log.e(TAG, head+message)
                     }
             }
         }
     }
+
+
+}
+
+fun Date.formatToServerTimeDefaults(): String{
+    val sdf= SimpleDateFormat("yy.MM.DD.hh.mm.ss", Locale.getDefault())
+    return sdf.format(this)
 }
